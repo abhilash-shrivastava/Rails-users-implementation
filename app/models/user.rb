15c759351da04761b7f426298587934d1f1ccb0a
abhilash shrivastava <abhilash.shrivastava@hotmail.com>
2015-10-24 21:29:34 -0700

    Show only active user

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,8 +40,7 @@ class User < ActiveRecord::Base
 
 
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: FILL_IN, activated_at: FILL_IN)
   end
 
   def send_activation_email
@@ -51,8 +50,9 @@ class User < ActiveRecord::Base
   # Sets the password reset attributes.
   def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(reset_digest:  FILL_IN,
+                   reset_sent_at: FILL_IN)
+
   end
 
   # Sends password reset email.
